e58b35518a87bc91f42a6ef530befe383c8260f0
EthanHueh <nattanssouzapvz@gmail.com>
2025-03-30 17:55:43 -0300

    feat: adição de algumas validações ao cliente

--- backend/src/main/java/com/comercios_sa/agenda/service/ClienteValidator.java
+++ backend/src/main/java/com/comercios_sa/agenda/service/ClienteValidator.java
@@ -1,6 +1,32 @@
 package com.comercios_sa.agenda.service;
 
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Component;
+
+import com.comercios_sa.agenda.model.Cliente;
+import com.comercios_sa.agenda.model.Contato;
+
+@Component
 public class ClienteValidator {
-    // TODO nome do usuario nao pode ser vazio
-    // TODO data de nascimento deve ser validada
+
+    public void validar(Cliente cliente) throws Exception{
+       
+        if (cliente.getNome().isBlank() || cliente.getNome().isEmpty()){
+            throw new Exception("Nome não pode ser vazio!");
+        }
+
+        if (cliente.getDataNascimento() != null && cliente.getDataNascimento().isAfter(LocalDate.now())){
+            throw new Exception("Data inválida!");
+        }
+
+        for (Contato contato : cliente.getContatos()) {
+            validar(contato);
+        }
+    }
+
+    private void validar(Contato contato) {
+        
+    }
+
 }
