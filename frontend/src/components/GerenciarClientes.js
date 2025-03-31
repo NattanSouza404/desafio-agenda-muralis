05e13a72c94a21959ee6f8c6efa295c1cea92aa3
EthanHueh <nattanssouzapvz@gmail.com>
2025-03-31 12:22:45 -0300

    feat: consultar clientes e remover clientes adicionado

--- frontend/src/components/GerenciarClientes.js
+++ frontend/src/components/GerenciarClientes.js
@@ -1,6 +1,32 @@
-import { consultarTodos } from '../api';
+import { useState, useEffect } from 'react';
+import { consultarTodos, removerCliente } from '../api';
 
 export default function GerenciarClientes() {
+
+    const [linhas, setLinhas] = useState(
+        []
+    );
+
+    useEffect(() => {
+        const obterDados = async () => {
+            const data = await consultarTodos();
+            setLinhas(data);
+        };
+
+        obterDados();
+    }, []); 
+
+    const removerLinha = async (indice) => {
+        // TODO: Tem certeza que quer deletar??
+
+        if (true){
+            await removerCliente(linhas[indice].id);
+
+            const linhasAtualizadas = linhas.filter((_, i) => i !== indice);
+            setLinhas(linhasAtualizadas);
+        }
+        
+    };
 
     return (
         <div className='GerenciarClientes'>
@@ -15,28 +41,24 @@ export default function GerenciarClientes() {
             <button onClick={() => consultarTodos()}>Pesquisar</button>
 
             <table id="resultados">
-                <tbody>
+                <thead>
                     <tr>
                         <th>Nome</th>
                         <th>CPF</th>
                         <th>Operações</th>
                     </tr>
-                    <tr>
-                        <td>a</td>
-                        <td>a</td>
-                        <td>
-                            <button>Editar</button>
-                            <button>Remover</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>a</td>
-                        <td>a</td>
-                        <td>
-                            <button>Editar</button>
-                            <button>Remover</button>
-                        </td>
-                    </tr>
+                </thead>
+                <tbody>
+                    {linhas.map((linha, indice) => (
+                        <tr key={indice}>
+                            <td>{linha.nome}</td>
+                            <td>{linha.cpf}</td>
+                            <td>
+                                <button>Editar</button>
+                                <button onClick={() => removerLinha(indice)}>Remover</button>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
