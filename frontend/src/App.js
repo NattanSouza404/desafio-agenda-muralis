bae0310d5fec7a43ab901b9f530ec83c6f4f69ab
EthanHueh <nattanssouzapvz@gmail.com>
2025-03-31 16:02:11 -0300

    feat: adição de rotas para o frontend e adição do componente Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
+import CadastroCliente from './components/CadastroCliente';
+import EditarCliente from './components/EditarCliente';
+import Footer from './components/Footer';
 import GerenciarClientes from './components/GerenciarClientes';
 import Navbar from './components/Navbar';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <hr/>
-      <GerenciarClientes/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <main>
+          <Routes>
+            <Route path="/" element={<GerenciarClientes />} />
+            <Route path="/cadastrarCliente" element={<CadastroCliente />} />
+            <Route path="/editarCliente" element={<EditarCliente />} />
+          </Routes>
+        </main>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
