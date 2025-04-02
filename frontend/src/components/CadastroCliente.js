0decd8e9f9831d8035a2b31c028cd7ad5a5b0b24
EthanHueh <nattanssouzapvz@gmail.com>
2025-04-02 12:00:16 -0300

    feat: melhorias de estilo na tela de cadastro do cliente

--- frontend/src/components/CadastroCliente.js
+++ frontend/src/components/CadastroCliente.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { cadastrarCliente } from "../api.js";
-import { Button, FormGroup, MenuItem, Select, TextField, Typography } from "@mui/material";
+import { Box, Button, Card, CardActions, CardContent, FormControl, Grid, InputLabel, MenuItem, Select, Stack, TextField, Typography } from "@mui/material";
 
 export default function CadastroCliente() {
 
@@ -49,73 +49,94 @@ export default function CadastroCliente() {
     };
 
     return (
-        <div className='CadastroCliente'>
-
-            <Typography variant="p">{cliente.nome}</Typography>
-            <FormGroup>
-                <Typography variant="p">Cliente</Typography>
-
-                <TextField type="text" name="nome" value={cliente.nome}
-                    label="Nome"
-                    onChange={(e) => atualizarCampo(e)}
-                />
-
-                <TextField type="text" name="cpf" value={cliente.cpf}
-                    label="CPF"
-                    onChange={(e) => atualizarCampo(e)}
-                />
-
-                <TextField
-                    type="date"
-                    name="dataNascimento"
-                    label="Data de Nascimento"
-                    value={cliente.dataNascimento}
-                    onChange={(e) => atualizarCampo(e)}
-                    slotProps={{ inputLabel: { shrink: true } }}
-                />
-
-
-                <TextField type="text" name="endereco"
-                    label="Endereço"
-                    onChange={(e) => atualizarCampo(e)}
-                />
-            </FormGroup>
-
-            <Typography variant="h3">Contato</Typography>
-
-            {
-                contatos.map((contato, index) => (
-                    <FormGroup key={index}>
-                        <Select name="tipoContato" 
-                            onChange={(e) => atualizarContato(index, e)}
-                            label="Tipo do Contato"
-                            required
-                        >
-                            <MenuItem value="TELEFONE">Telefone</MenuItem>
-                            <MenuItem value="EMAIL">E-mail</MenuItem>
-                        </Select>
-
-                        <TextField
-                            type="text"
-                            label="Contato"
-                            value={contato.valor}
-                            name="valor"
-                            onChange={(e) => atualizarContato(index, e)}
-                        />
-
-                        <TextField type="text" name="observacao"
-                            label="Observação"
-                            onChange={(e) => atualizarContato(index, e)}
-                        />
-
-                        <Button onClick={() => removerContato(contato)}>Remover</Button>
-                    </FormGroup>
-                ))
-            }
-
-            <Button onClick={() => adicionarNovoContato()}>Adicionar novo contato</Button>
-
+        <Box className='CadastroCliente' alignItems={"center"} >
             <Button onClick={() => enviarFormulario()}>Salvar</Button>
-        </div>
+            <Grid container spacing={4}>
+
+                <Grid size={6} justifyContent={"center"}>
+                    <Typography variant="h6">Cliente</Typography>
+
+                    <Card spacing={4}>
+                        <CardContent>
+                            <Stack spacing={4}>
+                                <TextField type="text" name="nome" value={cliente.nome}
+                                    label="Nome"
+                                    onChange={(e) => atualizarCampo(e)}
+                                    required
+                                />
+
+                                <TextField type="text" name="cpf" value={cliente.cpf}
+                                    label="CPF"
+                                    onChange={(e) => atualizarCampo(e)}
+                                    required
+                                    helperText="Formato xxx.xxx.xxx-xx"
+                                />
+
+                                <TextField
+                                    type="date"
+                                    name="dataNascimento"
+                                    label="Data de Nascimento"
+                                    value={cliente.dataNascimento}
+                                    onChange={(e) => atualizarCampo(e)}
+                                    slotProps={{ inputLabel: { shrink: true } }}
+                                />
+
+                                <TextField type="text" name="endereco"
+                                    label="Endereço"
+                                    onChange={(e) => atualizarCampo(e)}
+                                />
+                            </Stack>
+                        </CardContent>
+
+                    </Card>
+
+                </Grid>
+
+                <Grid size={6}>
+                    <Typography variant="h6">Contatos</Typography>
+
+                    <Stack spacing={2}>
+                        {
+                            contatos.map((contato, index) => (
+                                <Card key={index}>
+                                    <CardContent>
+                                        <FormControl component={"form"} key={index}>
+                                            <InputLabel>Tipo do Contato</InputLabel>
+                                            <Select
+                                                name="tipoContato"
+                                                onChange={(e) => atualizarContato(index, e)}
+                                                label="Tipo do Contato"
+                                                required
+                                            >
+                                                <MenuItem value="TELEFONE">Telefone</MenuItem>
+                                                <MenuItem value="EMAIL">E-mail</MenuItem>
+                                            </Select>
+
+                                            <TextField
+                                                type="text"
+                                                label="Contato"
+                                                value={contato.valor}
+                                                name="valor"
+                                                required
+                                                onChange={(e) => atualizarContato(index, e)}
+                                            />
+
+                                            <TextField type="text" name="observacao"
+                                                label="Observação"
+                                                onChange={(e) => atualizarContato(index, e)}
+                                            />
+
+                                        </FormControl>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button onClick={() => removerContato(contato)}>Remover</Button>
+                                    </CardActions>
+                                </Card>
+                            ))}
+                        <Button onClick={() => adicionarNovoContato()}>Adicionar novo contato</Button>
+                    </Stack>
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
