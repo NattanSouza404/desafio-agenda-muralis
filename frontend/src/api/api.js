3a95b64765a4d62e19d5e08788605b27901689f1
EthanHueh <nattanssouzapvz@gmail.com>
2025-04-02 16:22:48 -0300

    feat: melhorias nas chamadas de api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,120 +1,126 @@
-export async function consultarTodos(){
+function estourarErro(msg, error) {
+    console.error(msg, error);
+    alert(msg, error);
+}
+
+export async function consultarTodos() {
     try {
         const response = await fetch("http://localhost:8080/cliente/listall");
         return await response.json();
     } catch (error) {
-        console.error('Erro buscando dados:', error);
+        estourarErro("Erro na consulta: ", error);
     }
-  }
+}
 
-export async function filtrarClientes(nome, cpf){
+export async function filtrarClientes(nome, cpf) {
     try {
-        const response = await fetch("http://localhost:8080/cliente/filter?nome="+nome+"&cpf="+cpf);
+        const response = await fetch("http://localhost:8080/cliente/filter?nome=" + nome + "&cpf=" + cpf);
         return await response.json();
     } catch (error) {
-        console.error('Erro buscando dados:', error);
+        estourarErro("Erro na consulta: ", error);
     }
 }
 
-export async function consultarById(id){
-  try {
-      const response = await fetch("http://localhost:8080/cliente/list/"+id);
-      return await response.json();
-  } catch (error) {
-      console.error('Erro buscando dados:', error);
-  }
+export async function consultarById(id) {
+    try {
+        const response = await fetch("http://localhost:8080/cliente/list/" + id);
+        return await response.json();
+    } catch (error) {
+        estourarErro("Erro ao consultar: ", error);
+    }
 }
 
-export async function cadastrarCliente(cliente){
+export async function cadastrarCliente(cliente) {
 
     try {
 
-        if (window.confirm("Deseja mesmo cadastrar esse cliente?")){
-        
-            const url = "http://localhost:8080/cliente/add";
-
-            const option = {
-                method: 'POST',
-                headers:{'Content-Type': 'application/json'},
-                body: JSON.stringify(cliente)
-            }
-
-            const result = await fetch(url, option);
-
-            if (result.status === 201) {
-                alert('Cadastrado com sucesso');
-            }
-            else {
-                const resposta = await result.json();
-                alert("Erro ao cadastrar: "+resposta.error);
-            }
+        if (!window.confirm("Deseja mesmo cadastrar esse cliente?")) {
+            return false;
         }
-        
+
+        const url = "http://localhost:8080/cliente/add";
+
+        const option = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(cliente)
+        }
+
+        const result = await fetch(url, option);
+
+        if (result.status === 201) {
+            alert('Cadastrado com sucesso');
+            return true;
+        }
+
+        const resposta = await result.json();
+        throw new Error(resposta.error);
+
     }
-    catch (error){
-        console.error('Erro ao cadastrar:', error);
+    catch (error) {
+        estourarErro("Erro no cadastro: ", error);
+        return false;
     }
 }
 
-export async function atualizarCliente(cliente, msg){
-    
+export async function atualizarCliente(cliente, msg) {
+
     try {
-        if (window.confirm(msg)){
-            const url = "http://localhost:8080/cliente/update";
+        if (!window.confirm(msg)) {
+            return false;
+        }
 
-            const option = {
-                method: 'PUT',
-                headers:{'Content-Type': 'application/json'},
-                body: JSON.stringify(cliente)
-            }
+        const url = "http://localhost:8080/cliente/update";
 
-            const result = await fetch(url, option);
+        const option = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(cliente)
+        }
 
-            if (result.status === 200) {
-                alert('Atualizado com sucesso!');
-            }
-            else {
-                const resposta = await result.json();
-                alert("Erro ao atualizar: "+resposta.error);
-            }
+        const result = await fetch(url, option);
 
+        if (result.status === 200) {
+            alert('Atualizado com sucesso!');
             return true;
         }
 
+        const resposta = await result.json();
+        throw new Error(resposta.error);
+
+    } catch (error) {
+        estourarErro("Erro ao atualizar: ", error);
         return false;
-    } catch (error){
-        console.error('Erro ao atualizar:', error);
     }
-    
+
 }
 
-export async function removerCliente(id){
-    
-    try { 
-        if (window.confirm("Deseja deletar esse cliente?")){
-            const url = 'http://localhost:8080/cliente/delete/'+id;
+export async function removerCliente(id) {
 
-            const response = await fetch(url, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
+    try {
+        if (!window.confirm("Deseja deletar esse cliente?")) {
+            return false;
+        }
+        const url = 'http://localhost:8080/cliente/delete/' + id;
 
-            if (response.status === 204) {
-                alert('Deletado com sucesso');
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
             }
-            else {
-                const resposta = await response.json();
-                alert("Erro ao deletar: "+resposta.error);
-            }
+        });
 
+        if (response.status === 204) {
+            alert('Deletado com sucesso');
             return true;
         }
 
+        const resposta = await response.json();
+        alert("Erro ao deletar: " + resposta.error);
+
+    } catch (error) {
+        estourarErro("Erro ao deleter: ", error);
         return false;
-    } catch (error){
-        console.error('Erro ao deletar:', error);
     }
-    
+
 }
